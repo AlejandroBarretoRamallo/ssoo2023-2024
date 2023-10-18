6df02c601b6585e3e966128afd01c1291764d2f0
alu0101552774 <114758777+AlejandroBarretoRamallo@users.noreply.github.com>
2023-10-18 08:39:15 +0100

    Add files via upload

---
+++ scdebug.sh
@@ -0,0 +1,54 @@
+UUID=$(uuidgen)
+case "$1" in
+  -sto )
+    argumento_sto="$2"
+    if [[ "$3" != "-nattch" ]]; then
+      myprog="$3"
+      PID=$(ps -eo pid,comm | grep "$myprog" | sort -n -r | head -n1 | awk '{print $1}')
+      if [ ! -d "./scdebug/" ];then
+        mkdir "./scdebug/"
+        mkdir "./scdebug/"$myprog""
+      fi
+      if [ ! -d "./scdebug/"$myprog"" ];then 
+        mkdir ./scdebug/"$myprog"
+      fi
+      strace $argumento_sto -o  ./scdebug/"$myprog"/trace_$UUID.txt $myprog &
+    fi
+    if [[ $3 == "-nattch" ]]; then
+      myprog="$4"
+      if [ ! -d "./scdebug/" ];then
+       mkdir "./scdebug/"
+       mkdir "./scdebug/"$myprog""
+      fi
+      if [ ! -d "./scdebug/"$myprog"" ];then 
+      mkdir ./scdebug/"$myprog"
+      fi
+      PID=$(ps -eo pid,comm | grep "$myprog" | sort -n -r | head -n1 | awk '{print $1}')
+      strace -c -p $PID -o  ./scdebug/"$myprog"/trace_$UUID.txt &
+    fi
+  ;;
+  * )
+    myprog="$1"
+    arg1="$2"
+    arg2="$3"
+    if [ ! -d "./scdebug/" ];then
+      mkdir "./scdebug/"
+      mkdir "./scdebug/"$myprog""
+    fi
+    if [ ! -d "./scdebug/"$myprog"" ];then 
+      mkdir ./scdebug/"$myprog"
+    fi
+    case "$#" in
+      2)
+        strace -o ./scdebug/$myprog/trace_$UUID.txt "$myprog" &
+      ;;
+      3)
+        strace -o ./scdebug/$myprog/trace_$UUID.txt "$myprog" "$arg1" &
+      ;;
+      4)
+        strace -o ./scdebug/$myprog/trace_$UUID.txt "$myprog" "$arg1" "$arg2" &
+      ;;
+    esac
+  ;;
+esac
+
